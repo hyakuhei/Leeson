d7227a5d3a5d40d8c6405d14a8c4717d6fb7acc4
Robert Clark <hyakuhei@gmail.com>
2015-07-17 15:07:51 +0100

    Added some error handling and logging for bad requests.

--- PecanBroker/pbroker/controllers/root.py
+++ PecanBroker/pbroker/controllers/root.py
@@ -1,9 +1,10 @@
 import pecan
 import logging
 
-from pecan import rest
+from pecan import rest, abort
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
 
 from database.dbmodel import Server, Volume
 
@@ -19,16 +20,24 @@ class KeyController(rest.RestController):
         uuid = pecan.request.GET.get('uuid')
         addr = pecan.request.remote_addr
         #XXX: Arggggg.
-        #This line allows the broker to positioned behind a TLS Load Balancer
+        #This allows the broker to positioned behind a TLS Load Balancer
         #However, I'm concerned that it could be abused in some configurations
         #Allowing some host to request key material for another host
         #Though that would still require the attacker to know a valid disk UUID
         if 'X-Forwarded-For' in pecan.request.headers:
                 addr = pecan.request.headers['X-Forwarded-For']
 
-        #TODO: Appropriate
-        server = session.query(Server).filter(Server.ip==addr).one()
-        vol = session.query(Volume).filter(Volume.server_id == server.id).filter(Volume.uuid==uuid).one()
+        try:
+            server = session.query(Server).filter(Server.ip==addr).one()
+        except NoResultFound, e:
+            logger.warning("%s not found inDB" % addr)
+            abort(404)
+
+        try:
+            vol = session.query(Volume).filter(Volume.server_id == server.id).filter(Volume.uuid==uuid).one()
+        except NoResultFound, e:
+            logger.warning("%s requested %s but no match in DB" % (addr,uuid))
+            abort(404)
 
         return vol.keymaterial
 
